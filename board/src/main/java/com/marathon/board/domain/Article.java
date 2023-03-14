b5e368d17c431d46a1fb72e1845c954e1f75cf85
wwwkang8 <wwwkang8@gmail.com>
2023-03-15 00:15:51 +0900

    설정파일 추가
    설정파일 및 도메인

--- board/src/main/java/com/marathon/board/domain/Article.java
+++ board/src/main/java/com/marathon/board/domain/Article.java
@@ -2,7 +2,16 @@ package com.marathon.board.domain;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table
 public class Article {
+
+  //본문 인덱스 : 본문검색에는 인덱스를 걸지 않는다. 너무 길어서 본문에는 인덱스 X. 그리고 인덱스에 용량이 한계가 있다.
   private long id;
   private String title; // 제목
 
