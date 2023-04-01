b35c71eafcb1ae66fdae85684cca1381d4d6340c
wwwkang8 <wwwkang8@gmail.com>
2023-04-01 14:54:00 +0900

    #1 - Querydsl 지원을 위해서 api 검색옵션 추가
    QuerydslPredicateExecutor, QuerydslBinderCustomizer를 이용하여 간단하게 구현

--- board/src/main/java/com/marathon/board/repository/ArticleCommentRepository.java
+++ board/src/main/java/com/marathon/board/repository/ArticleCommentRepository.java
@@ -1,9 +1,32 @@
 package com.marathon.board.repository;
 
+import com.marathon.board.domain.Article;
 import com.marathon.board.domain.ArticleComment;
+import com.marathon.board.domain.QArticle;
+import com.marathon.board.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+    JpaRepository<ArticleComment, Long>,
+    QuerydslPredicateExecutor<ArticleComment>,
+    QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        // 선택적으로 특정 필드에 대해서 검색 가능하게 하기 위한 장치
+        bindings.excludeUnlistedProperties(true); //일단 검색에서 제외하도록 하고
+        bindings.including(root.content, root.createdAt, root.modifiedAt); //이 필드들만 검색 대상
+        bindings.bind(root.content).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.createdAt).first((DateTimeExpression::eq)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.createdBy).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+
+    }
+
 }
--- board/src/main/java/com/marathon/board/repository/ArticleRepository.java
+++ board/src/main/java/com/marathon/board/repository/ArticleRepository.java
@@ -1,9 +1,36 @@
 package com.marathon.board.repository;
 
 import com.marathon.board.domain.Article;
+import com.marathon.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringExpressions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+    JpaRepository<Article, Long> ,
+    QuerydslPredicateExecutor<Article>,
+    QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // 선택적으로 특정 필드에 대해서 검색 가능하게 하기 위한 장치
+        // 인터페이스인데 인터페이스 내부에 메서드를 구현할 수 있다. => java8부터 가능
+        bindings.excludeUnlistedProperties(true); //일단 검색에서 제외하도록 하고
+        bindings.including(root.title, root.content, root.hashTag, root.createdAt, root.modifiedAt); //이 필드들만 검색 대상
+        bindings.bind(root.title).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.content).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.hashTag).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.createdAt).first((DateTimeExpression::eq)); // like '%value%' 쿼리를 생성한다
+        bindings.bind(root.createdBy).first((StringExpression::containsIgnoreCase)); // like '%value%' 쿼리를 생성한다
+
+    }
+
 }
+
