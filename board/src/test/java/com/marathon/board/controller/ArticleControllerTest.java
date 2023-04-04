243bc1dd1dcd4dedc39d93622b0f936149653ca2
wwwkang8 <wwwkang8@gmail.com>
2023-04-04 18:56:26 +0900

    #15 - 게시판 뷰 기본 골격 생성
    컨트롤러 핸들러 메소드로 테스트를 하나 통과하도록 생성
    뷰 적절히 뼈대 생성
    테스트에서는 media type검사에 대해서 부가적인 옵션이 들어갈 수 있기 때문에 테스트 메서드 수정

--- board/src/test/java/com/marathon/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/marathon/board/controller/ArticleControllerTest.java
@@ -26,7 +26,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 {게시판} 페이지 - 정상 호출")
     @Test
     public void given_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -35,12 +34,11 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
-            .andExpect(view().name("/articles/index"))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/index"))
             .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void given_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -55,7 +53,6 @@ class ArticleControllerTest {
             .andExpect(model().attributeExists("articleComments"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색전용페이지 - 정상 호출")
     @Test
     public void given_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
@@ -68,7 +65,6 @@ class ArticleControllerTest {
             .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색페이지 - 정상 호출")
     @Test
     public void given_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
