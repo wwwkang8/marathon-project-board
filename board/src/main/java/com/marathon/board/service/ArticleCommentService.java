3c6a87d1642c843101a7976e67a8d30577c6b438
wwwkang8 <wwwkang8@gmail.com>
2023-05-24 21:14:09 +0900

    댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ board/src/main/java/com/marathon/board/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.marathon.board.service;
+
+import java.util.List;
+
+import com.marathon.board.dto.ArticleCommentDto;
+import com.marathon.board.repository.ArticleCommentRepository;
+import com.marathon.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
--- board/src/test/java/com/marathon/board/service/ArticleServiceTest.java
+++ board/src/test/java/com/marathon/board/service/ArticleServiceTest.java
@@ -1,9 +1,13 @@
 package com.marathon.board.service;
 
 import com.marathon.board.domain.Article;
+import com.marathon.board.domain.ArticleComment;
+import com.marathon.board.domain.UserAccount;
 import com.marathon.board.domain.type.SearchType;
+import com.marathon.board.dto.ArticleCommentDto;
 import com.marathon.board.dto.ArticleDto;
 import com.marathon.board.dto.ArticleUpdateDto;
+import com.marathon.board.repository.ArticleCommentRepository;
 import com.marathon.board.repository.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,83 +15,53 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
-@DisplayName("비즈니스 로직 - 게시글")
+@DisplayName("비즈니스 로직 - 댓글")
 @ExtendWith(MockitoExtension.class)
-class ArticleServiceTest {
+class ArticleCommentServiceTest {
 
-    @InjectMocks private ArticleService sut;
+    @InjectMocks private ArticleCommentService sut;
 
     @Mock private ArticleRepository articleRepository;
+    @Mock private ArticleCommentRepository articleCommentRepository;
 
-    @DisplayName("게시글을 검색하면, 게시글 리스트를 반환한다.")
+    @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
-    void givenSearchParameters_whenSearchingArticles_thenReturnsArticleList() {
+    void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
         // Given
+        Long articleId = 1L;
+        UserAccount userAccount = UserAccount.of("sdf", "12312", "jake@gmail.com","jd","asdf");
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(
+            Article.of(userAccount,"title", "content", "#java"))
+        );
 
         // When
-        Page<ArticleDto> articles = sut.searchArticles(SearchType.TITLE, "search keyword");
+        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
 
         // Then
-        assertThat(articles).isNotNull();
+        assertThat(articleComments).isNotNull();
+        then(articleRepository).should().findById(articleId);
     }
 
-    @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
     @Test
-    void givenArticleId_whenSearchingArticle_thenReturnsArticle() {
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
         // Given
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
         // When
-        ArticleDto articles = sut.searchArticle(1L);
+        sut.saveArticleComment(ArticleCommentDto.of(LocalDateTime.now(), "Uno", LocalDateTime.now(), "Uno", "comment"));
 
         // Then
-        assertThat(articles).isNotNull();
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
-
-    @DisplayName("게시글 정보를 입력하면, 게시글을 생성한다")
-    @Test
-    void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
-        // Given
-        given(articleRepository.save(any(Article.class))).willReturn(null);
-
-        // When
-        sut.saveArticle(ArticleDto.of(LocalDateTime.now(), "Uno", "title", "content", "#java"));
-
-        // Then
-        then(articleRepository).should().save(any(Article.class));
-    }
-
-    @DisplayName("게시글의 ID와 수정 정보를 입력하면, 게시글을 수정한다")
-    @Test
-    void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdatesArticle() {
-        // Given
-        given(articleRepository.save(any(Article.class))).willReturn(null);
-
-        // When
-        sut.updateArticle(1L, ArticleUpdateDto.of("title", "content", "#java"));
-
-        // Then
-        then(articleRepository).should().save(any(Article.class));
-    }
-
-    @DisplayName("게시글의 ID를 입력하면, 게시글을 삭제한다")
-    @Test
-    void givenArticleId_whenDeletingArticle_thenDeletesArticle() {
-        // Given
-        willDoNothing().given(articleRepository).delete(any(Article.class));
-
-        // When
-        sut.deleteArticle(1L);
-
-        // Then
-        then(articleRepository).should().delete(any(Article.class));
-    }
-
 }
