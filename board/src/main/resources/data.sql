7f99ed8b71391e0e7a0057795e533f6df05ec80d
wwwkang8 <wwwkang8@gmail.com>
2023-06-18 15:31:25 +0900

    #29.3-게시판, 게시글 뷰에 필요한 내용 추가
    1) 게시글 상세페이지 detail.html 추가
    2) 게시글 상세페이지 내의 데이터 뿌려주는 thymeleaf 추가
    3) data.sql 수정.

--- board/src/main/resources/data.sql
+++ board/src/main/resources/data.sql
@@ -8,7 +8,7 @@ insert into user_account (user_id, user_password, nickname, email, memo, created
 ;
 
 -- 123 게시글
-insert into article (user_id, title, content, hashtag, created_by, modified_by, created_at, modified_at) values
+insert into article (user_id, title, content, hash_tag, created_by, modified_by, created_at, modified_at) values
 ('uno2', 'Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
 
 Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
