64ac3dc5adb929b5cb66e8976472031efcf1c421
wwwkang8 <wwwkang8@gmail.com>
2023-07-30 17:18:43 +0900

    #62-대댓글 도메인 설계
    대댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드 추가
    자식 댓글의 컬렉션 변화가 쿼리에 반영되게끔
    cascading 규칙을 모두 적용
    이번엔 단방향 연관관계 설정을 사용해보기로 함.
    따라서 부모 댓글은 엔티티가 아닌 'Long' id를 직접 표현
    
    또한 자식댓글을 추가할 수 있는 메서드 추가 제공

--- src/main/java/com/marathon/board/domain/ArticleComment.java
+++ src/main/java/com/marathon/board/domain/ArticleComment.java
@@ -1,8 +1,12 @@
 package com.marathon.board.domain;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -12,6 +16,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -47,6 +53,16 @@ public class ArticleComment extends AuditingFields {
   private UserAccount userAccount; // 유저 정보 (ID)
 
   @Setter
+  @Column(updatable = false) // 업데이트 대상에서 제외한다. 부모댓글이 바뀌는 일은 없다.
+  private Long parentCommentId; // 부모댓글 ID. 단방향으로 하기 위해서 Id로 직접 설정.
+
+  @ToString.Exclude
+  @OrderBy("createdAt ASC")
+  @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL) // 부모 자식댓글이 긴밀해서 cascade 규칙을 모두 열었다.
+  private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+
+  @Setter
   @Column(nullable = false, length = 500)
   private String content; // 내용
 
@@ -54,14 +70,21 @@ public class ArticleComment extends AuditingFields {
   protected ArticleComment() {
   }
 
-  private ArticleComment(Article article, UserAccount userAccount, String content) {
+  private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
     this.article = article;
     this.userAccount = userAccount;
+    this.parentCommentId = parentCommentId;
     this.content = content;
   }
 
+  /** 도메인이 변경되어도 of 메서드로 인해서 객체생성 하는데에 변경되는 영향도를 차단할 수 있다. */
   public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-    return new ArticleComment(article, userAccount, content);
+    return new ArticleComment(article, userAccount, null, content);
+  }
+
+  public void addChildComment(ArticleComment child) {
+    child.setParentCommentId(this.getId());
+    this.getChildComments().add(child);
   }
 
   @Override
